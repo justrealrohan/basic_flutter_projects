3e4f99d8b0f0f9d11b52c6d7fa1e24c79a76d007
rohansmac <rohan@webit.life>
2023-12-24 12:19:42 +0600

    dice_roller_app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'gradient_container.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Dice App',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.5),
+          ),
+        ),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: const GradientContainer(),
+    ),
+  ));
+}
